4c693db38c1eec7d5b4aaea5df9a39f6398ca30e
unknown <rodrigo.hirashiki@uhsp.edu>
2025-09-07 21:23:48 -0500

    Debugged final output issues

--- product_catalog.py
+++ product_catalog.py
@@ -1,27 +1,28 @@
 from product_data import products
 # TODO: Step 1 - Print out the products to see the data that you are working with.
 
-
+print(products)
 
 # TODO: Step 2 - Create a list called customer_preferences and store the user preference in this list.
 
+customer_preferences = []
 
 response = ""
 while response != "N":
     print("Input a preference:")
     preference = input()
     # Add the customer preference to the list
-
+    customer_preferences.append(preference)
     response = input("Do you want to add another preference? (Y/N): ").upper()
   
 
 # TODO: Step 3 - Convert customer_preferences list to set to eliminate duplicates.
 
-
+customer_preferences = set(customer_preferences)
 
 # TODO: Step 4 - Convert the product tags to sets in order to allow for faster comparisons.
-converted_products = []
-
+for product in products:
+    product['tags'] = set(product['tags'])
 
 
 
@@ -34,7 +35,7 @@ def count_matches(product_tags, customer_tags):
     Returns:
         int: The number of matching tags between the product and customer.
     '''
-    pass
+    return len(product_tags & customer_tags)
 
 
 
@@ -48,13 +49,22 @@ def recommend_products(products, customer_tags):
     Returns:
         list: A list of products containing product names and their match counts.
     '''
-    pass
+    recommend_products=[]
+    for product in products:
+        matches = count_matches(product['tags'], customer_tags)
+        recommend_products.append({'name': product['name'], 'matches': matches})
+    recommend_products.sort(key=lambda x: x['matches'], reverse=True)
+    return recommend_products
 
 
 
 # TODO: Step 7 - Call your function and print the results
 
+results = recommend_products(products, customer_preferences)
 
+print("Recommended Products:")
+for result in results:
+    print(f"{result['name']}: {result['matches']} matches")
 
 
 # DESIGN MEMO (write below in a comment):
